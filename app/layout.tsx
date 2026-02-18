39eefe9ad92b66cab7da6b4ea9b12042924050ea
Marcoo2003 <marcozanchin@MacBook-Pro-di-Marco.local>
2026-02-18 11:55:15 +0100

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SystemHeader from "./components/SystemHeader";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -45,6 +45,7 @@ export default function RootLayout({
         
         <SystemHeader />
         {children}
+        <Analytics />
       </body>
     </html>
   );
